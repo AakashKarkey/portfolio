1d8283c9a5156aa1eba29df1ee9841a055fa79e8
Aakash Karki <yaakash543@gmail.com>
2025-06-29 15:35:37 +0545

    cv download button updatedd

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -121,17 +121,16 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1fybUYda4ldDYG4svBWcJUrspwbgrmAIG/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block text-white py-3 px-8 rounded-full text-lg font-bold transition duration-300 transform hover:scale-105"
-            style={{
-              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
-              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
-            }}
-          >
-            DOWNLOAD CV
-          </a>
+  href="/AakashKarkiCV.pdf"
+  download="Aakash_Karki_CV.pdf"
+  className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+  style={{
+    background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+    boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+  }}
+>
+  DOWNLOAD CV
+</a>
         </div>
 
         {/* Right Section */}
